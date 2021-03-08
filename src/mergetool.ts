a7cc9d2767aa69a38cd55b2a081062f4aaad0c88
Muhammad Numan <muhammadnuman70@gmail.com>
2021-03-08 23:20:40 +0500

    feat: add support to detect conflict in file

--- src/mergetool.ts
+++ src/mergetool.ts
@@ -1,9 +1,24 @@
 import { window } from 'vscode';
-import { NOT_FOUND_CONFLICT_ERROR, FOUND_CONFLICT_ERROR, isThisFileHaveConflict } from './util/util';
+const { exec } = require('child_process');
+import {
+	NOT_FOUND_CONFLICT_ERROR,
+	FOUND_CONFLICT_ERROR,
+	NOT_FOUND_INTELLIJ_ERROR,
+	isThisFileHaveConflict
+} from './util/util';
+const COMMAND="/Applications/WebStorm.app/Contents/MacOS/webstorm merge";
 
 export const openIntelliJMergeTool = (filePath: string) => {
 	if (isThisFileHaveConflict(filePath)) {
-		window.showInformationMessage(FOUND_CONFLICT_ERROR);
+        window.showInformationMessage(FOUND_CONFLICT_ERROR);
+        const EXEC_COMMAND=COMMAND+` ${filePath} ${filePath} ${filePath} ${filePath}`;
+		exec(EXEC_COMMAND, [], function(err: null | boolean, stdout: string, stderr: string) {
+			if (err) {
+                console.log(err,"err")
+				window.showErrorMessage(NOT_FOUND_INTELLIJ_ERROR);
+				return;
+			}
+		});
 	} else {
 		window.showErrorMessage(NOT_FOUND_CONFLICT_ERROR);
 	}
--- src/util/util.ts
+++ src/util/util.ts
@@ -2,9 +2,10 @@ const fs = require('fs');
 
 export const NOT_FOUND_CONFLICT_ERROR = 'This file has no merge conflict';
 export const FOUND_CONFLICT_ERROR = 'This file has conflict';
+export const NOT_FOUND_INTELLIJ_ERROR = 'You computer have no intelli j';
 export const isThisFileHaveConflict = (filePath: string) => {
 	const fileContent = fs.readFileSync(filePath, 'utf8');
-	const regexDetectMergeConflicts = /<<<<<<< HEAD(\n+.+)\n\=\=\=\=\=\=/g;
+	const regexDetectMergeConflicts = /(<<<<<<< HEAD)(.*)(\=\=\=\=\=\=\=)/s;
 	const isHaveConflictThisFile = regexDetectMergeConflicts.test(fileContent);
 	return isHaveConflictThisFile;
 };
