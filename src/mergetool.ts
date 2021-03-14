dd83518ec6d177235f265a1cdaa17eec7756c103
Muhammad Numan <muhammadnuman70@gmail.com>
2021-03-14 23:01:55 +0500

    fix: project root path

--- src/mergetool.ts
+++ src/mergetool.ts
@@ -1,9 +1,8 @@
-import { window } from 'vscode';
+import { window,workspace } from 'vscode';
 const { exec } = require('child_process');
 import {
 	NOT_FOUND_CONFLICT_ERROR,
 	FOUND_CONFLICT_ERROR,
-	NOT_FOUND_INTELLIJ_ERROR,
 	isThisFileHaveConflict,
 	getMasterSlaveBranch,
 	getFileNameFromFilePath,
@@ -15,19 +14,18 @@ const COMMAND="/Applications/WebStorm.app/Contents/MacOS/webstorm merge";
 export const openIntelliJMergeTool = async (filePath: string) => {
 	if (isThisFileHaveConflict(filePath)) {
         window.showInformationMessage(FOUND_CONFLICT_ERROR);
-		const projectRootPath:string="/Users/apple/Data/test/node/Horizon_Preview_Apps";
+		const projectRootPath:any=workspace.rootPath;
 		const masterBranchName:string=getMasterSlaveBranch("master",projectRootPath);
 		const slaveBranchName:string=getMasterSlaveBranch("slave",projectRootPath);
 		const fileName=getFileNameFromFilePath(filePath);
 		const masterFileRelativePath=await makeFileWithCommand(masterBranchName,filePath,projectRootPath,fileName,"master");
 		const slaveFileRelativePath=await makeFileWithCommand(slaveBranchName,filePath,projectRootPath,fileName,"slave");
         const EXEC_COMMAND:string=COMMAND+` ${masterFileRelativePath} ${slaveFileRelativePath} ${filePath}`;
-		exec(EXEC_COMMAND, [], function(err: null | boolean, stdout: string, stderr: string) {
+		exec(EXEC_COMMAND, [], function(err: null | boolean | any, stdout: string, stderr: string) {
 			removeFileByPath(masterFileRelativePath);
 			removeFileByPath(slaveFileRelativePath);
 			if (err) {
-				console.log(err,"err");
-				window.showErrorMessage(NOT_FOUND_INTELLIJ_ERROR);
+				window.showErrorMessage(err);
 				return;
 			}
 		});
