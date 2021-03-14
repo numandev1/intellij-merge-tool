17378c49f9c01b0b4d1e90aba140872d5aa125e2
Muhammad Numan <muhammadnuman70@gmail.com>
2021-03-14 20:35:33 +0500

    feat: add support for merge file by webstorm

--- src/mergetool.ts
+++ src/mergetool.ts
@@ -4,20 +4,32 @@ import {
 	NOT_FOUND_CONFLICT_ERROR,
 	FOUND_CONFLICT_ERROR,
 	NOT_FOUND_INTELLIJ_ERROR,
-	isThisFileHaveConflict
+	isThisFileHaveConflict,
+	getMasterSlaveBranch,
+	getFileNameFromFilePath,
+	makeFileWithCommand,
+	removeFileByPath
 } from './util/util';
 const COMMAND="/Applications/WebStorm.app/Contents/MacOS/webstorm merge";
 
-export const openIntelliJMergeTool = (filePath: string) => {
+export const openIntelliJMergeTool = async (filePath: string) => {
 	if (isThisFileHaveConflict(filePath)) {
         window.showInformationMessage(FOUND_CONFLICT_ERROR);
-        const EXEC_COMMAND=COMMAND+` ${filePath} ${filePath} ${filePath} ${filePath}`;
+		const projectRootPath:string="/Users/apple/Data/test/node/Horizon_Preview_Apps";
+		const masterBranchName:string=getMasterSlaveBranch("master",projectRootPath);
+		const slaveBranchName:string=getMasterSlaveBranch("slave",projectRootPath);
+		const fileName=getFileNameFromFilePath(filePath);
+		const masterFileRelativePath=await makeFileWithCommand(masterBranchName,filePath,projectRootPath,fileName,"master");
+		const slaveFileRelativePath=await makeFileWithCommand(slaveBranchName,filePath,projectRootPath,fileName,"slave");
+        const EXEC_COMMAND:string=COMMAND+` ${masterFileRelativePath} ${slaveFileRelativePath} ${filePath}`;
 		exec(EXEC_COMMAND, [], function(err: null | boolean, stdout: string, stderr: string) {
 			if (err) {
-                console.log(err,"err")
+				console.log(err,"err");
 				window.showErrorMessage(NOT_FOUND_INTELLIJ_ERROR);
 				return;
 			}
+			removeFileByPath(masterFileRelativePath);
+			removeFileByPath(slaveFileRelativePath);
 		});
 	} else {
 		window.showErrorMessage(NOT_FOUND_CONFLICT_ERROR);
--- src/util/util.ts
+++ src/util/util.ts
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const path = require('path');
+const { exec } = require('child_process');
 
 export const NOT_FOUND_CONFLICT_ERROR = 'This file has no merge conflict';
 export const FOUND_CONFLICT_ERROR = 'This file has conflict';
@@ -8,4 +10,39 @@ export const isThisFileHaveConflict = (filePath: string) => {
 	const regexDetectMergeConflicts = /(<<<<<<< HEAD)(.*)(\=\=\=\=\=\=\=)/s;
 	const isHaveConflictThisFile = regexDetectMergeConflicts.test(fileContent);
 	return isHaveConflictThisFile;
+};
+export const getMasterSlaveBranch = (branchType: string, rootPath: string): string => {
+	const masterBranchNameRegex = /(?<=remote-tracking branch '.+' into ).+/;
+	const slaveBranchNameRegex = /(?<=remote-tracking branch ').+(?=')/;
+	const filePath = path.resolve(rootPath, '.git/MERGE_MSG');
+	const fileContent = fs.readFileSync(filePath, 'utf8');
+	if (branchType === 'master') {
+		return masterBranchNameRegex.exec(fileContent) + '';
+	} else {
+		return slaveBranchNameRegex.exec(fileContent) + '';
+	}
+};
+export const getFileNameFromFilePath = (filePath: string) => {
+	const fileNameRegex = /[^/]+$/;
+	return fileNameRegex.exec(filePath) + '';
+};
+
+export const makeFileWithCommand=async (branchName:string,filePath:string,rootPath:string,fileName:string,branchType:string)=>{
+	return new Promise((resolve,rejects)=>{
+		const relativeFilePath=path.relative(rootPath,filePath);
+		const newFileName=`.temp.${branchType}.${fileName}`;
+		const EXEC_COMMAND:string=`cd ${rootPath} && git show ${branchName}:${relativeFilePath} > ${newFileName}`;
+		exec(EXEC_COMMAND, [], function(err: null | boolean, stdout: string, stderr: string) {
+			if (err) {
+				console.log(err,"err");
+				rejects("");
+				return;
+			}
+			resolve(path.resolve(rootPath,newFileName));
+		});
+	});
+};
+
+export const removeFileByPath=(filePath:any)=>{
+	fs.unlinkSync(filePath);
 };
